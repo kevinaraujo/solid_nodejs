0e5c03bee80062ceb960cceeba14633fd39853b0
Kevin Araújo <kfa_34@hotmail.com>
2021-04-19 16:15:56 -0300

    Used Single principle to save userinto database, Liskov substitution principle and Dependency inversion principle.

---
+++ README.md
@@ -0,0 +1,3 @@
+## SOLID using nodeJS and TypeScript
+
+This is one project of mine about study I did to know how implement SOLID in nodeJS.
---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { Router } from "express"; 
+
+const router = Router()
+
+router.post('/users', (req, res) => {
+  return res.status(201).send();
+})
+
+export { router }
